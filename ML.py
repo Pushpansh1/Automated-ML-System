8ff65d8e8a40c6de79f44b3f5e6c1a0f2bd02f67
Gourav Singh Thakur <144212296+GouravSinghThakur@users.noreply.github.com>
2025-02-27 19:12:25 +0530

    Update ML.py

--- ML.py
+++ ML.py
@@ -95,7 +95,7 @@ def get_model_configs():
         },
         'XgBoost':{
             'pipeline':Pipeline([
-            ('scaled', StandardScaler()),
+            ('scaled',StandardScaler()),
             ('classifier',XGBClassifier(use_label_encoder=False,eval_metric='logloss'))
             ]),
             'params':{
